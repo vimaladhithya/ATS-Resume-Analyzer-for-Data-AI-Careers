3ca62e4d4e04c0d40e2d7da0166a5a61a18a31c4
A. P. Vimal Adhithya <vimazhadhi@gmail.com>
2026-06-11 21:30:24 +0530

    Fix NLTK punkt_tab error and update preprocessing

--- nltk_setup.py
+++ nltk_setup.py
@@ -4,6 +4,7 @@ def download_nltk_resources():
     resources = [
         ("corpora/stopwords", "stopwords"),
         ("tokenizers/punkt", "punkt"),
+        ("tokenizers/punkt_tab", "punkt_tab"),
         ("corpora/wordnet", "wordnet"),
         ("corpora/omw-1.4", "omw-1.4")
     ]
